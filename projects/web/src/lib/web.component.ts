4aba45b5e249105c86ef7da6bda27d8141728ae4
Your Name <you@example.com>
2020-10-22 09:12:09 +0300

    initial commit

---
+++ projects/web/src/lib/web.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-web',
+  template: `
+    <p>
+      web works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class WebComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
