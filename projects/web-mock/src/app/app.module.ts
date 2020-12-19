f942deb50e5f6e6d7224a22e07345d6d2c5fdc18
joshua mshana <mama27j@gmail.com>
2020-12-19 16:09:05 +0300

    update angular version

--- projects/web-mock/src/app/app.module.ts
+++ projects/web-mock/src/app/app.module.ts
@@ -27,7 +27,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
     RouterModule,
     BrowserAnimationsModule,
     MatCardModule,
