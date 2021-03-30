31e7868cf084ab535ad17a994a37e2391790478a
davkimfray <davkimfray@gmail.com>
2021-03-30 16:37:23 +0300

    landing page privacy.page.ts

--- projects/web/src/pages/landing.page.ts
+++ projects/web/src/pages/landing.page.ts
@@ -1,135 +1,118 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, HostListener} from '@angular/core';
 import {Router} from '@angular/router';
 import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-landing',
   template: `
-    <div style="background: #d1f4e6">
-      <nav style="top: 0; z-index: 10000" class="navbar position-sticky navbar-expand-lg navbar-light shadow p-3 "
-           style="background: #d1f4e6">
+    <div class="bg-white w-100 p-0 m-0">
+      <div class="mask-text">
+        <h1 class="header-text text-center"></h1>
+      </div>
+      <nav [ngClass]="animateNavBar ? 'hide-nav-bar' : ''" class="navbar w-100 position-fixed navbar-expand-lg navbar-light shadow p-3 ">
         <div class="container">
-
           <a class="navbar-brand" href="">
             SmartStock
           </a>
-
-          <!--  <button class="navbar-toggler" type="button"-->
-          <!--          data-toggle="collapse"-->
-          <!--          data-target="#navbarSupportedContent"-->
-          <!--          aria-controls="navbarSupportedContent"-->
-          <!--          aria-expanded="false"-->
-          <!--          aria-label="Toggle navigation">-->
-          <!--    <span class="navbar-toggler-icon"></span>-->
-          <!--  </button>-->
-
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
               <li class="nav-item">
                 <a class="nav-link" href="#why-us">Why Us</a>
               </li>
-              <!--      <li class="nav-item">-->
-              <!--        <a class="nav-link" href="#testimonies">Testimonies</a>-->
-              <!--      </li>-->
               <li class="nav-item">
                 <a class="nav-link" href="#pricing">Pricing</a>
               </li>
             </ul>
           </div>
           <span class="flex-grow-1"></span>
-          <button routerLink="/account/login" class="btn btn-outline-success">Login</button>
-          <span style="width: 10px; height: 10px"></span>
+          <button routerLink="/account/login" class="btn btn-outline-success mx-2">Login</button>
           <button routerLink="/account/register" class="btn btn-outline-success">Register</button>
         </div>
       </nav>
-
-      <section class="header-sm header d-flex flex-column container">
-        <div class="header-img header-jumbo flex-grow-1 container d-flex align-items-start justify-content-center flex-column">
-
-        </div>
-        <div class="row">
-          <div class="header-jumbo flex-grow-1 container d-flex align-items-start justify-content-center flex-column">
-            <h1 class="header-text">AI Powered Sales and Stock Management Software...</h1>
-          </div>
-
-        </div>
-        <div class="header-sub-text">
-          <p style="line-height: 4vh">
-            You can’t continue using papers <br> to keep records of your business.
-          <p>
-          <p style="padding-top: 2vh; padding-bottom: 2vh">Start your sales with us, today</p>
-          <button mat-flat-button color="primary" routerLink="/account/register" class=" header-btn">Open Account For Free</button>
-<!--          <div style="width: 20px; height: 20px; display: inline-block"></div>-->
-<!--          <button color="primary" mat-flat-button routerLink="/account/login" class="header-btn">Login</button>-->
-        </div>
-      </section>
-
-      <section class="header-lg header d-flex flex-column container">
-        <div class="row">
-          <div class="col-6 header-jumbo flex-grow-1 container d-flex align-items-start justify-content-center flex-column">
-            <h1 class="header-text">AI Powered Sales and Stock Management Software...</h1>
-            <div class="header-sub-text">
-              <p style="line-height: 4vh">
-                You can’t continue using papers <br> to keep records of your business.
-              <p>
-              <p style="padding-top: 2vh; padding-bottom: 2vh">Start your sales with us, today</p>
-              <button mat-flat-button color="primary" routerLink="/account/register" class="header-btn">Open Account For Free</button>
-<!--              <div style="width: 20px; height: 20px; display: inline-block"></div>-->
-<!--              <button color="primary" mat-flat-button routerLink="/account/login" class="header-btn">Login</button>-->
+      <div id="header" class="header">
+        <section class="header-sm d-flex flex-column container ">
+          <div class="header-img flex-grow-1 container d-flex align-items-start justify-content-center flex-column"></div>
+          <div class="row m-0">
+            <div class="flex-grow-1 container d-flex align-items-start justify-content-center flex-column">
+              <h1 class="header-text">AI Powered Sales and Stock Management Software...</h1>
             </div>
           </div>
-          <div class="col-6 header-img header-jumbo flex-grow-1 container d-flex align-items-start flex-column">
-
+          <div class="header-sub-text pl-4">
+            <p style="line-height: 4vh">
+              You can’t continue using papers <br> to keep records of your business.
+            <p>
+            <p class="py-3">Start your sales with us, today</p>
+            <button mat-flat-button color="primary" routerLink="/account/register" class=" header-btn">Open Account For
+              Free
+            </button>
           </div>
-        </div>
-      </section>
-
-      <section style="padding-top: 16px " class="why-smartstock" id="why-us">
-        <div class="container">
-          <h1>Why Smartstock?</h1>
-          <mat-divider></mat-divider>
+        </section>
+        <section class="header-lg d-flex flex-column container ">
           <div class="row">
-            <div class="col-md-3 text-center">
+            <div
+              class="col-6 header-jumbo flex-grow-1 container d-flex align-items-start justify-content-center flex-column">
+              <h1 class="header-text">AI Powered Sales and Stock Management Software...</h1>
+              <div class="header-sub-text">
+                <p style="line-height: 4vh">
+                  You can’t continue using papers <br> to keep records of your business.
+                <p>
+                <p style="padding-top: 2vh; padding-bottom: 2vh">Start your sales with us, today</p>
+                <button mat-flat-button color="primary" routerLink="/account/register" class="header-btn">Open Account
+                  For Free
+                </button>
+              </div>
+            </div>
+            <div class="col-6 header-img flex-grow-1 container d-flex align-items-start flex-column"></div>
+          </div>
+        </section>
+      </div>
+      <section class="why-smartstock" id="why-us" [ngClass]="animateWhyUsDiv ? 'visible' : ''">
+        <div class="section-header ">
+          <h1>Why SmartStock?</h1>
+          <ul class="list-unstyled d-flex justify-content-center align-items-center">
+            <li *ngFor="let x of [1,2,3,4]"></li>
+          </ul>
+        </div>
+        <div class="container ">
+          <div class="row my-md-5 my-4">
+            <div class="col-sm-6 col-md-4 px-lg-5 my-4 text-center ">
               <img src="/assets/img/everyWhere_icon.png">
-              <h4>Work everywhere</h4>
+              <h2>Work everywhere</h2>
               <p>
                 You can access our system either by <b>desktop / laptop</b>, <b>mobile</b> or <b>web browser</b>
               </p>
             </div>
-            <div class="col-md-3 text-center">
-              <img src="/assets/img/“99.9%25”.png" width="50%" style="padding: 40px 10px">
-              <h4>Always available</h4>
+            <div class="col-sm-6 col-md-4 px-lg-5 my-4 text-center">
+              <img src="/assets/img/“99.9%25”.png" width="50%" class="py-5">
+              <h2>Always available</h2>
               <p>
                 Due to internet connectivity challenge
                 our service available offline and online
               </p>
             </div>
-            <div class="col-md-3 text-center">
+            <div class="col-sm-6 col-md-4 px-lg-5 my-4 text-center">
               <img src="/assets/img/customisable_icon.png">
-              <h4>Customizable</h4>
+              <h2>Customizable</h2>
               <p>Say how you want it to work for you
                 and it will work the way you want</p>
             </div>
-          </div>
-
-          <div class="row">
-            <div class="col-md-3 text-center">
+            <div class="col-sm-6 col-md-4 px-lg-5 my-4 text-center">
               <img src="/assets/img/everyWhere_icon.png">
-              <h4>Customer Engagement</h4>
+              <h2>Customer Engagement</h2>
               <p>
                 Create your online shop for customer to make order and see your products.
               </p>
             </div>
-            <div class="col-md-3 text-center">
-              <img src="/assets/img/customisable_icon.png" width="50%" style="padding: 19px 10px">
-              <h4>Business Intelligence</h4>
+            <div class="col-sm-6 col-md-4 px-lg-5 my-4 text-center">
+              <img src="/assets/img/customisable_icon.png" width="47%" class="p-3">
+              <h2>Business Intelligence</h2>
               <p>
                 Artificial Intelligence Algorithms to provide business insights to increase sales.
               </p>
             </div>
-            <div class="col-md-3 text-center">
+            <div class="col-sm-6 col-md-4 px-lg-5 my-4 text-center">
               <img src="/assets/img/receipt.svg">
-              <h4>Printer</h4>
+              <h2>Printer</h2>
               <p>
                 Printer is integrated by default. Your customer deserve a receipt.
               </p>
@@ -138,119 +121,80 @@ import {FormControl} from '@angular/forms';
 
         </div>
       </section>
-
-      <section style="padding-top: 36px; background: #d1f4e6" id="pricing">
-        <div class="container">
-          <h1>Pricing</h1>
-          <mat-divider></mat-divider>
-          <div style="margin-top: 60px" class="container">
-
-            <div style="" class="row">
-              <div
-                class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 box-shadow d-flex flex-column align-items-start justify-content-center">
-                <div class="btn-block">
-                  <div class="card-deck mb-3 text-center">
-                    <div class="card mb-4  shadow  card-pricing">
-                      <div class="card-header">
-                        <h4 class="my-0 font-weight-normal">All In One Pack</h4>
-                      </div>
-                      <div class="card-body">
-                        <h1 class="card-title pricing-card-title">TSH 30,000 <small class="text-muted">/ month / shop</small></h1>
-                        <ul class="list-unstyled mt-3 mb-4">
-                          <li>Unlimited Users</li>
-                          <li>Online Access</li>
-                          <li>Offline Access</li>
-                          <li>Printer and Scanner Access</li>
-                          <li>Intelligence Pack</li>
-                          <li>E-commerce outlet for online order</li>
-                          <li>Help center and training access</li>
-                        </ul>
-<!--                        <button style="background: #00AA07; color: white" routerLink="/account/register" type="button"-->
-<!--                                class="btn btn-lg btn-block">Get Started-->
-<!--                        </button>-->
-                      </div>
-                    </div>
-<!--                    <div class="card mb-4 shadow card-pricing">-->
-<!--                      <div class="card-header">-->
-<!--                        <h4 class="my-0 font-weight-normal">Pro</h4>-->
-<!--                      </div>-->
-<!--                      <div class="card-body">-->
-<!--                        <h1 class="card-title pricing-card-title">TSH 50,000 <small class="text-muted">/ mo</small></h1>-->
-<!--                        <ul class="list-unstyled mt-3 mb-4">-->
-<!--                          <li>4 users included</li>-->
-<!--                          <li>2 shops</li>-->
-<!--                          <li>Online Access</li>-->
-<!--                          <li>Help center and training access</li>-->
-<!--                        </ul>-->
-<!--                        <button style="background: #00AA07; color: white" routerLink="/account/register" type="button"-->
-<!--                                class="btn btn-lg btn-block">Get started-->
-<!--                        </button>-->
-<!--                      </div>-->
-<!--                    </div>-->
-<!--                    <div class="card mb-4 shadow card-pricing">-->
-<!--                      <div class="card-header">-->
-<!--                        <h4 class="my-0 font-weight-normal">Medium</h4>-->
-<!--                      </div>-->
-<!--                      <div class="card-body">-->
-<!--                        <h1 class="card-title pricing-card-title">TSH 70,000 <small class="text-muted">/ mo</small></h1>-->
-<!--                        <ul class="list-unstyled mt-3 mb-4">-->
-<!--                          <li>6 users included</li>-->
-<!--                          <li>3 shops</li>-->
-<!--                          <li>Online Access</li>-->
-<!--                          <li>Help center and training access</li>-->
-<!--                        </ul>-->
-<!--                        <button style="background: #00AA07; color: white" routerLink="/account/register" type="button"-->
-<!--                                class="btn btn-lg btn-block ">Get started-->
-<!--                        </button>-->
-<!--                      </div>-->
-<!--                    </div>-->
-<!--                    <div class="card mb-4 shadow card-pricing">-->
-<!--                      <div class="card-header">-->
-<!--                        <h4 class="my-0 font-weight-normal">Enterprise</h4>-->
-<!--                      </div>-->
-<!--                      <div class="card-body">-->
-<!--                        <h1 class="card-title pricing-card-title">TSH 100,000 <small class="text-muted">/ mo</small></h1>-->
-<!--                        <ul class="list-unstyled mt-3 mb-4">-->
-<!--                          <li>Unlimited Users</li>-->
-<!--                          <li>5 shops</li>-->
-<!--                          <li>Online Access</li>-->
-<!--                          <li>Help center and training access</li>-->
-<!--                        </ul>-->
-<!--                        <button style="background: #00AA07; color: white" routerLink="/account/register" type="button"-->
-<!--                                class="btn btn-lg btn-block">Get started-->
-<!--                        </button>-->
-<!--                      </div>-->
-<!--                    </div>-->
-                  </div>
-
-                </div>
-              </div>
-
-              <button style="margin-top: 24px" mat-button color="primary" routerLink="/account/register"
-                      class="col-12 header-btn-secondary shadow">
-                Open Account For Free
-              </button>
-
+      <div class="pricing-container py-5" [ngClass]="animatePricingDiv ? 'visible' : ''">
+        <section id="pricing" (window:scroll)="onScroll($event)" class="text-center  py-2 w-100">
+          <div>
+            <div class="section-header">
+              <h1>Pricing Plans</h1>
+              <ul class="list-unstyled d-flex justify-content-center align-items-center">
+                <li *ngFor="let x of [1,2,3,4]"></li>
+              </ul>
             </div>
-
-            <app-footer></app-footer>
-
+            <p>Pay for what you use only</p>
+            <p>Monthly Pricing
+              <mat-slide-toggle [(ngModel)]="activateDiscount">Annual Pricing (10% discount)</mat-slide-toggle>
+            </p>
           </div>
-        </div>
-      </section>
+          <div class="row pt-3 mx-5 container justify-content-center align-items-center pricing-body">
+            <div class="col-11 col-md-8 col-lg-6 left-div bg-white mat-elevation-z24 m-0 p-0 ">
+              <h1 class="mb-0 p-4">All In One Pack</h1>
+              <hr class="mt-0">
+              <div class="d-flex flex-column align-items-center">
+                <p class="p-2">
+                  <span class="mat-display-1">TZS {{activateDiscount ? 30000 - 30000 * 0.1 : '30000'}}</span><br>
+                  <span class="text-muted">/ month / shop</span>
+                </p>
+                <ul class="list-unstyled my-2 mx-4 mx-sm-auto">
+                  <li>Unlimited Users</li>
+                  <li>Online Access</li>
+                  <li>Offline Access</li>
+                  <li>Printer and Scanner Access</li>
+                  <li>Intelligence Pack</li>
+                  <li>E-commerce outlet for online order</li>
+                  <li>Help center and training access</li>
+                </ul>
+              </div>
+              <hr class="mb-0">
+              <button mat-flat-button color="primary" routerLink="/account/register" class="m-3 header-btn">Open Account
+                For Free
+              </button>
+            </div>
+            <div class="col-10 col-md-7 col-lg-6 right-div bg-success text-white mat-elevation-z24 m-0 p-0 ">
+              <h1 class="col-9 col-md-12 mx-auto my-2 my-md-4 text-white">Start Your Free 7-Day Trial</h1>
+              <hr class="mt-0 bg-white">
+              <p class="col-9 col-lg-6 m-auto py-2 py-md-4">
+                Need More than what we offer?
+              </p>
+              <p class="col-9 col-lg-6 m-auto py-2 py-md-4">
+                We can customize it just the way you want!
+              </p>
+              <hr class="mb-0">
+              <h2 class="link mb-0 p-4 text-white">CONTACT US</h2>
+            </div>
+          </div>
+        </section>
+      </div>
+      <div id="footer" class="container">
+        <app-footer></app-footer>
+      </div>
     </div>
   `,
   styleUrls: ['../styles/landing.style.scss']
 })
 export class LandingPage implements OnInit {
+  animateWhyUsDiv = false;
+  animateNavBar = false;
+  lastScroll = 0;
 
   constructor(private router: Router) {
   }
 
+  activateDiscount = false;
   currencyFormControl = new FormControl('TZS');
   totalProducts = 1;
   totalSales = 1;
   totalPurchases = 1;
+  animatePricingDiv = false;
 
 
   ngOnInit(): void {
@@ -288,7 +232,7 @@ export class LandingPage implements OnInit {
     }
   }
 
-  actualMonthlyCost() {
+  actualMonthlyCost(): any {
     return this.calculateBill(30, this.totalSales, this.totalPurchases, this.totalProducts, this.currencyFormControl.value);
   }
 
@@ -309,5 +253,25 @@ export class LandingPage implements OnInit {
     const stockCost = 0.0003 * days * exRate * products;
     const analyticsCost = 0.01 * days * exRate;
     return (salesCost + purchaseCost + stockCost + analyticsCost);
+  }
+
+  onScroll(event: Event): void {
+    const pricingHeight = Math.round(document.getElementById('pricing').offsetHeight);
+    const whyUsHeight = Math.round(document.getElementById('why-us').offsetHeight);
+    this.animateWhyUsDiv = Math.round(document.getElementById('why-us').getBoundingClientRect().y) <= whyUsHeight - (whyUsHeight / 4);
+    this.animatePricingDiv = Math.round(document.getElementById('pricing').getBoundingClientRect().y) <=
+      pricingHeight - (pricingHeight / 4);
+
+    const currentY = window.pageYOffset;
+
+    if (currentY > this.lastScroll) {
+      if (currentY > this.lastScroll + 25) {
+        this.animateNavBar = true;
+        this.lastScroll = currentY;
+      }
+    } else {
+      this.lastScroll = currentY;
+      this.animateNavBar = false;
+    }
   }
 }
--- projects/web/src/web.module.ts
+++ projects/web/src/web.module.ts
@@ -12,6 +12,8 @@ import {MatOptionModule} from '@angular/material/core';
 import {MatSelectModule} from '@angular/material/select';
 import {BrowserPlatformGuard} from './guards/browser-platform.guard';
 import {MatButtonModule} from '@angular/material/button';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatListModule} from '@angular/material/list';
 
 const routes: Routes = [
   {
@@ -28,27 +30,29 @@ const routes: Routes = [
     FooterComponent,
     PrivacyPageComponent
   ],
-  imports: [
-    CommonModule,
-    {
-      providers: [
+    imports: [
+        CommonModule,
         {
-          multi: true,
-          provide: ROUTES,
-          useValue: routes
-        }
-      ],
-      ngModule: RouterModule
-    },
-    MatDividerModule,
-    FormsModule,
-    MatCardModule,
-    MatSliderModule,
-    MatOptionModule,
-    MatSelectModule,
-    ReactiveFormsModule,
-    MatButtonModule
-  ],
+            providers: [
+                {
+                    multi: true,
+                    provide: ROUTES,
+                    useValue: routes
+                }
+            ],
+            ngModule: RouterModule
+        },
+        MatDividerModule,
+        FormsModule,
+        MatCardModule,
+        MatSliderModule,
+        MatOptionModule,
+        MatSelectModule,
+        ReactiveFormsModule,
+        MatButtonModule,
+        MatSlideToggleModule,
+        MatListModule
+    ],
 })
 export class WebModule {
 }
